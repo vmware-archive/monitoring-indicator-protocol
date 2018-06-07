1637c221fbafe012fba88a383a00339e756b1165
Joseph Rodriguez <jrodriguez@pivotal.io>
2018-06-07 14:21:56 -0600

    randomize tmp build location
    Signed-off-by: David Timm <dtimm@pivotal.io>

--- pkg/vgo_test/build.go
+++ pkg/vgo_test/build.go
@@ -3,15 +3,21 @@ package vgo_test
 import (
 	"os/exec"
 	"fmt"
+	"io/ioutil"
 )
 
 func Build(packagePath string, args ...string) (compiledPath string, err error) {
-	build := exec.Command("vgo", "build", "-o", "/tmp/build", packagePath)
+	tmpDir, err := ioutil.TempDir("", "gexec_artifacts")
+	if err != nil {
+		return "", err
+	}
+
+	build := exec.Command("vgo", "build", "-o", tmpDir + "/build", packagePath)
 
 	output, err := build.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("Failed to build %s:\n\nError:\n%s\n\nOutput:\n%s", packagePath, err, string(output))
 	}
 
-	return "/tmp/build", nil
+	return tmpDir + "/build", nil
 }
