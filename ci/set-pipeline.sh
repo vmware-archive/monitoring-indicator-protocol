4a30c44961fc7eaaaf874dea1e185e9bf89f5e2b
David Timm <dtimm@pivotal.io>
2018-08-21 15:29:11 -0600

    Move CI scripts to repo
    - add push-to-acceptance step to CI
    
    Signed-off-by: Milan Klanjsek <mklanjsek@pivotal.io>

---
+++ ci/set-pipeline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ef
+
+if [[ $# -ne 1 ]];
+then
+    echo "Incorrect usage: $0 pipeline-name"
+fi
+
+set -eu
+
+lpass ls > /dev/null # check that we're logged in
+
+echo setting pipeline for "$1"
+fly -t superpipe set-pipeline -p "$1" \
+    -c "pipelines/$1.yml" \
+    --load-vars-from <(lpass show --notes "Shared-Event Producer (Pivotal Only)/concourse.yml")
