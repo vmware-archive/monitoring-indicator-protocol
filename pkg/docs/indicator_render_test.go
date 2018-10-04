87e425a03ed7fb6098b558e254b9c6c73080e8f9
David Timm <dtimm@pivotal.io>
2018-10-04 13:12:04 -0600

    make threshold section optional for indicators with none

--- pkg/docs/indicator_render_test.go
+++ pkg/docs/indicator_render_test.go
@@ -1,61 +1,85 @@
 package docs_test
 
 import (
-  "testing"
+	"testing"
 
-  . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 
-  "code.cloudfoundry.org/indicators/pkg/docs"
-  "code.cloudfoundry.org/indicators/pkg/indicator"
+	"code.cloudfoundry.org/indicators/pkg/docs"
+	"code.cloudfoundry.org/indicators/pkg/indicator"
 )
 
 func TestRenderIndicatorHTML(t *testing.T) {
-  g := NewGomegaWithT(t)
+	t.Run("it renders indicators", func(t *testing.T) {
+		g := NewGomegaWithT(t)
 
-  indicator := indicator.Indicator{
-    Name: "test_indicator",
-    Documentation: map[string]string{
-      "title":       "Test Indicator",
-      "description": "*test description* of kpi",
-      "recommended_response":    "*test response* of kpi",
-      "measurement": "Average over 100 minutes",
-      "threshold_note": "dynamic!",
-    },
-    PromQL: `avg_over_time(test_latency{source_id="test"}[100m])`,
+		indicator := indicator.Indicator{
+			Name: "test_indicator",
+			Documentation: map[string]string{
+				"title":                "Test Indicator",
+				"description":          "*test description* of kpi",
+				"recommended_response": "*test response* of kpi",
+				"measurement":          "Average over 100 minutes",
+				"threshold_note":       "dynamic!",
+			},
+			PromQL: `avg_over_time(test_latency{source_id="test"}[100m])`,
 
-    Thresholds: []indicator.Threshold{
-      {
-        Level:    "warning",
-        Operator: indicator.GreaterThan,
-        Value:    500,
-      },
-      {
-        Level:    "critical",
-        Operator: indicator.GreaterThan,
-        Value:    1000,
-      },
-      {
-        Level:    "super_green",
-        Operator: indicator.LessThan,
-        Value:    10,
-      },
-    },
-  }
+			Thresholds: []indicator.Threshold{
+				{
+					Level:    "warning",
+					Operator: indicator.GreaterThan,
+					Value:    500,
+				},
+				{
+					Level:    "critical",
+					Operator: indicator.GreaterThan,
+					Value:    1000,
+				},
+				{
+					Level:    "super_green",
+					Operator: indicator.LessThan,
+					Value:    10,
+				},
+			},
+		}
 
-  html, err := docs.IndicatorToHTML(indicator)
-  g.Expect(err).ToNot(HaveOccurred())
+		html, err := docs.IndicatorToHTML(indicator)
+		g.Expect(err).ToNot(HaveOccurred())
 
-  g.Expect(html).To(ContainSubstring(`### <a id="test_indicator"></a>Test Indicator`))
-  g.Expect(html).To(ContainSubstring("<p><em>test description</em> of kpi</p>"))
-  g.Expect(html).To(ContainSubstring(`<code>avg_over_time(test_latency{source_id="test"}[100m])</code>`))
+		g.Expect(html).To(ContainSubstring(`### <a id="test_indicator"></a>Test Indicator`))
+		g.Expect(html).To(ContainSubstring("<p><em>test description</em> of kpi</p>"))
+		g.Expect(html).To(ContainSubstring(`<code>avg_over_time(test_latency{source_id="test"}[100m])</code>`))
 
-  g.Expect(html).To(ContainSubstring("<p><em>test response</em> of kpi</p>"))
-  g.Expect(html).To(ContainSubstring("<p>Average over 100 minutes</p>"))
-  g.Expect(html).To(ContainSubstring("<em>Red critical</em>: &gt; 1000<br/>"))
-  g.Expect(html).To(ContainSubstring("<em>Yellow warning</em>: &gt; 500<br/>"))
-  g.Expect(html).To(ContainSubstring("<em>super_green</em>: &lt; 10<br/>"))
-  g.Expect(html).To(ContainSubstring("dynamic!"))
-  g.Expect(html).To(ContainSubstring("Recommended Response"))
+		g.Expect(html).To(ContainSubstring("<p><em>test response</em> of kpi</p>"))
+		g.Expect(html).To(ContainSubstring("<p>Average over 100 minutes</p>"))
+		g.Expect(html).To(ContainSubstring("<em>Red critical</em>: &gt; 1000<br/>"))
+		g.Expect(html).To(ContainSubstring("<em>Yellow warning</em>: &gt; 500<br/>"))
+		g.Expect(html).To(ContainSubstring("<em>super_green</em>: &lt; 10<br/>"))
+		g.Expect(html).To(ContainSubstring("dynamic!"))
+		g.Expect(html).To(ContainSubstring("Recommended Response"))
 
-  g.Expect(html).ToNot(ContainSubstring("%%"))
+		g.Expect(html).ToNot(ContainSubstring("%%"))
+	})
+
+	t.Run("it skips the threshold section when absent", func(t *testing.T) {
+		g:= NewGomegaWithT(t)
+
+		indicator := indicator.Indicator{
+			Name: "test_indicator",
+			Documentation: map[string]string{
+				"title":                "Test Indicator",
+				"description":          "*test description* of kpi",
+				"recommended_response": "*test response* of kpi",
+				"measurement":          "Average over 100 minutes",
+			},
+			PromQL: `avg_over_time(test_latency{source_id="test"}[100m])`,
+
+			Thresholds: []indicator.Threshold{},
+		}
+
+		html, err := docs.IndicatorToHTML(indicator)
+		g.Expect(err).ToNot(HaveOccurred())
+
+		g.Expect(html).ToNot(ContainSubstring("<th>Thresholds</th>"))
+	})
 }
