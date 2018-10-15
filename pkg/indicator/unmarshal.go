8046f9b7be9c28188efb91c2b77b8b5328b561bf
Dennis Collinson <dcollinson@pivotal.io>
2018-10-15 16:28:03 -0600

    parse SLO from indicator yaml
    Signed-off-by: David Timm <dtimm@pivotal.io>

--- pkg/indicator/unmarshal.go
+++ pkg/indicator/unmarshal.go
@@ -1,195 +1,204 @@
 package indicator
 
 import (
-  "bytes"
-  "fmt"
-  "strconv"
+	"bytes"
+	"fmt"
+	"strconv"
 
-  "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 )
 
 func ReadIndicatorDocument(yamlBytes []byte, overrideMetadata ...map[string]string) (Document, error) {
-  metadata, err := readMetadata(yamlBytes)
-  parsedYAMLBytes := fillInMetadata(metadata, overrideMetadata, yamlBytes)
+	metadata, err := readMetadata(yamlBytes)
+	parsedYAMLBytes := fillInMetadata(metadata, overrideMetadata, yamlBytes)
 
-  var d yamlDocument
+	var d yamlDocument
 
-  err = yaml.Unmarshal(parsedYAMLBytes, &d)
-  if err != nil {
-    return Document{}, fmt.Errorf("could not unmarshal indicators: %s", err)
-  }
+	err = yaml.Unmarshal(parsedYAMLBytes, &d)
+	if err != nil {
+		return Document{}, fmt.Errorf("could not unmarshal indicators: %s", err)
+	}
 
-  var indicators []Indicator
-  for _, yamlIndicator := range d.Indicators {
-    var thresholds []Threshold
-    for _, yamlThreshold := range yamlIndicator.Thresholds {
-      threshold, err := thresholdFromYAML(yamlThreshold)
-      if err != nil {
-        return Document{}, fmt.Errorf("could not convert yaml indicators to indicators: %s", err)
-      }
+	var indicators []Indicator
+	for idx, yamlIndicator := range d.Indicators {
+		var thresholds []Threshold
+		for _, yamlThreshold := range yamlIndicator.Thresholds {
+			threshold, err := thresholdFromYAML(yamlThreshold)
+			if err != nil {
+				return Document{}, fmt.Errorf("could not convert yaml indicators to indicators: %s", err)
+			}
 
-      thresholds = append(thresholds, threshold)
-    }
+			thresholds = append(thresholds, threshold)
+		}
 
-    indicators = append(indicators, Indicator{
-      Name:          yamlIndicator.Name,
-      PromQL:        yamlIndicator.Promql,
-      Thresholds:    thresholds,
-      Documentation: yamlIndicator.Documentation,
-    })
-  }
+		var slo float64
+		if yamlIndicator.SLO != "" {
+			slo, err = strconv.ParseFloat(yamlIndicator.SLO, 64)
+			if err != nil {
+				return Document{}, fmt.Errorf("indicators[%d] slo unparsable: %s\n", idx, err)
+			}
+		}
 
-  var sections []Section
-  for idx, s := range d.YAMLDoc.Sections {
+		indicators = append(indicators, Indicator{
+			Name:          yamlIndicator.Name,
+			PromQL:        yamlIndicator.Promql,
+			Thresholds:    thresholds,
+			SLO:           slo,
+			Documentation: yamlIndicator.Documentation,
+		})
+	}
 
-    var sectionIndicators []Indicator
-    for iIdx, i := range s.IndicatorRefs {
-      indic, ok := findIndicator(i, indicators)
-      if !ok {
-        return Document{}, fmt.Errorf("documentation.sections[%d].indicators[%d] references non-existent indicator", idx, iIdx)
-      }
+	var sections []Section
+	for idx, s := range d.YAMLDoc.Sections {
 
-      sectionIndicators = append(sectionIndicators, indic)
-    }
+		var sectionIndicators []Indicator
+		for iIdx, i := range s.IndicatorRefs {
+			indic, ok := findIndicator(i, indicators)
+			if !ok {
+				return Document{}, fmt.Errorf("documentation.sections[%d].indicators[%d] references non-existent indicator", idx, iIdx)
+			}
 
-    sections = append(sections, Section{
-      Title:       s.Title,
-      Description: s.Description,
-      Indicators:  sectionIndicators,
-    })
-  }
+			sectionIndicators = append(sectionIndicators, indic)
+		}
 
-  documentation := Documentation{
-    Title:       d.YAMLDoc.Title,
-    Description: d.YAMLDoc.Description,
-    Sections:    sections,
-    Owner:       d.YAMLDoc.Owner,
-  }
+		sections = append(sections, Section{
+			Title:       s.Title,
+			Description: s.Description,
+			Indicators:  sectionIndicators,
+		})
+	}
 
-  return Document{
-    APIVersion:    d.APIVersion,
-    Product:       d.Product,
-    Version:       d.Version,
-    Metadata:      d.Metadata,
-    Indicators:    indicators,
-    Documentation: documentation,
-  }, nil
+	documentation := Documentation{
+		Title:       d.YAMLDoc.Title,
+		Description: d.YAMLDoc.Description,
+		Sections:    sections,
+		Owner:       d.YAMLDoc.Owner,
+	}
+
+	return Document{
+		APIVersion:    d.APIVersion,
+		Product:       d.Product,
+		Version:       d.Version,
+		Metadata:      d.Metadata,
+		Indicators:    indicators,
+		Documentation: documentation,
+	}, nil
 }
 
 type yamlDocument struct {
-  APIVersion string            `yaml:"apiVersion"`
-  Product    string            `yaml:"product"`
-  Version    string            `yaml:"version"`
-  Metadata   map[string]string `yaml:"metadata"`
-  Indicators []yamlIndicator   `yaml:"indicators"`
-  YAMLDoc    yamlDocumentation `yaml:"documentation"`
+	APIVersion string            `yaml:"apiVersion"`
+	Product    string            `yaml:"product"`
+	Version    string            `yaml:"version"`
+	Metadata   map[string]string `yaml:"metadata"`
+	Indicators []yamlIndicator   `yaml:"indicators"`
+	YAMLDoc    yamlDocumentation `yaml:"documentation"`
 }
 
 type yamlDocumentation struct {
-  Title       string        `yaml:"title"`
-  Description string        `yaml:"description"`
-  Sections    []yamlSection `yaml:"sections"`
-  Owner       string        `yaml:"owner"`
+	Title       string        `yaml:"title"`
+	Description string        `yaml:"description"`
+	Sections    []yamlSection `yaml:"sections"`
+	Owner       string        `yaml:"owner"`
 }
 
 type yamlSection struct {
-  Title         string   `yaml:"title"`
-  Description   string   `yaml:"description"`
-  IndicatorRefs []string `yaml:"indicators"`
+	Title         string   `yaml:"title"`
+	Description   string   `yaml:"description"`
+	IndicatorRefs []string `yaml:"indicators"`
 }
 
 type yamlIndicator struct {
-  Name          string            `yaml:"name"`
-  Promql        string            `yaml:"promql"`
-  Thresholds    []yamlThreshold   `yaml:"thresholds"`
-  SLO           string            `yaml:"slo"`
-  Documentation map[string]string `yaml:"documentation"`
+	Name          string            `yaml:"name"`
+	Promql        string            `yaml:"promql"`
+	Thresholds    []yamlThreshold   `yaml:"thresholds"`
+	SLO           string            `yaml:"slo"`
+	Documentation map[string]string `yaml:"documentation"`
 }
 
 type yamlThreshold struct {
-  Level   string `yaml:"level"`
-  Dynamic bool   `yaml:"dynamic"`
-  LT      string `yaml:"lt"`
-  LTE     string `yaml:"lte"`
-  EQ      string `yaml:"eq"`
-  NEQ     string `yaml:"neq"`
-  GTE     string `yaml:"gte"`
-  GT      string `yaml:"gt"`
+	Level   string `yaml:"level"`
+	Dynamic bool   `yaml:"dynamic"`
+	LT      string `yaml:"lt"`
+	LTE     string `yaml:"lte"`
+	EQ      string `yaml:"eq"`
+	NEQ     string `yaml:"neq"`
+	GTE     string `yaml:"gte"`
+	GT      string `yaml:"gt"`
 }
 
 func findIndicator(name string, indicators []Indicator) (Indicator, bool) {
-  for _, i := range indicators {
-    if i.Name == name {
-      return i, true
-    }
-  }
+	for _, i := range indicators {
+		if i.Name == name {
+			return i, true
+		}
+	}
 
-  return Indicator{}, false
+	return Indicator{}, false
 }
 
 func thresholdFromYAML(threshold yamlThreshold) (Threshold, error) {
-  var operator OperatorType
-  var value float64
-  var err error
+	var operator OperatorType
+	var value float64
+	var err error
 
-  switch {
-  case threshold.LT != "":
-    operator = LessThan
-    value, err = strconv.ParseFloat(threshold.LT, 64)
-  case threshold.LTE != "":
-    operator = LessThanOrEqualTo
-    value, err = strconv.ParseFloat(threshold.LTE, 64)
-  case threshold.EQ != "":
-    operator = EqualTo
-    value, err = strconv.ParseFloat(threshold.EQ, 64)
-  case threshold.NEQ != "":
-    operator = NotEqualTo
-    value, err = strconv.ParseFloat(threshold.NEQ, 64)
-  case threshold.GTE != "":
-    operator = GreaterThanOrEqualTo
-    value, err = strconv.ParseFloat(threshold.GTE, 64)
-  case threshold.GT != "":
-    operator = GreaterThan
-    value, err = strconv.ParseFloat(threshold.GT, 64)
-  default:
-    return Threshold{}, fmt.Errorf("could not find threshold value: one of [lt, lte, eq, neq, gte, gt] must be provided as a float")
-  }
+	switch {
+	case threshold.LT != "":
+		operator = LessThan
+		value, err = strconv.ParseFloat(threshold.LT, 64)
+	case threshold.LTE != "":
+		operator = LessThanOrEqualTo
+		value, err = strconv.ParseFloat(threshold.LTE, 64)
+	case threshold.EQ != "":
+		operator = EqualTo
+		value, err = strconv.ParseFloat(threshold.EQ, 64)
+	case threshold.NEQ != "":
+		operator = NotEqualTo
+		value, err = strconv.ParseFloat(threshold.NEQ, 64)
+	case threshold.GTE != "":
+		operator = GreaterThanOrEqualTo
+		value, err = strconv.ParseFloat(threshold.GTE, 64)
+	case threshold.GT != "":
+		operator = GreaterThan
+		value, err = strconv.ParseFloat(threshold.GT, 64)
+	default:
+		return Threshold{}, fmt.Errorf("could not find threshold value: one of [lt, lte, eq, neq, gte, gt] must be provided as a float")
+	}
 
-  if err != nil {
-    return Threshold{}, err
-  }
+	if err != nil {
+		return Threshold{}, err
+	}
 
-  return Threshold{
-    Level:    threshold.Level,
-    Operator: operator,
-    Value:    value,
-  }, nil
+	return Threshold{
+		Level:    threshold.Level,
+		Operator: operator,
+		Value:    value,
+	}, nil
 }
 
 func readMetadata(document []byte) (map[string]string, error) {
-  var d struct {
-    Metadata map[string]string `yaml:"metadata"`
-  }
+	var d struct {
+		Metadata map[string]string `yaml:"metadata"`
+	}
 
-  err := yaml.Unmarshal(document, &d)
-  if err != nil {
-    return nil, fmt.Errorf("could not unmarshal metadata: %s", err)
-  }
+	err := yaml.Unmarshal(document, &d)
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal metadata: %s", err)
+	}
 
-  return d.Metadata, nil
+	return d.Metadata, nil
 }
 
 func fillInMetadata(documentMetadata map[string]string, overrideMetadata []map[string]string, documentBytes []byte) []byte {
 
-  for _, overrides := range overrideMetadata {
-    for k, v := range overrides {
-      documentMetadata[k] = v
-    }
-  }
+	for _, overrides := range overrideMetadata {
+		for k, v := range overrides {
+			documentMetadata[k] = v
+		}
+	}
 
-  for k, v := range documentMetadata {
-    documentBytes = bytes.Replace(documentBytes, []byte("$"+k), []byte(v), -1)
-  }
+	for k, v := range documentMetadata {
+		documentBytes = bytes.Replace(documentBytes, []byte("$"+k), []byte(v), -1)
+	}
 
-  return documentBytes
+	return documentBytes
 }
--- pkg/indicator/unmarshal_test.go
+++ pkg/indicator/unmarshal_test.go
@@ -1,15 +1,15 @@
 package indicator_test
 
 import (
-  . "github.com/onsi/gomega"
-  "testing"
+	. "github.com/onsi/gomega"
+	"testing"
 
-  "code.cloudfoundry.org/indicators/pkg/indicator"
+	"code.cloudfoundry.org/indicators/pkg/indicator"
 )
 
 func TestReturnsCompleteDocument(t *testing.T) {
-  g := NewGomegaWithT(t)
-  d, err := indicator.ReadIndicatorDocument([]byte(`---
+	g := NewGomegaWithT(t)
+	d, err := indicator.ReadIndicatorDocument([]byte(`---
 apiVersion: v0
 product: well-performing-component
 version: 0.0.1
@@ -37,77 +37,78 @@ documentation:
     indicators:
     - test_performance_indicator
 `))
-  g.Expect(err).ToNot(HaveOccurred())
+	g.Expect(err).ToNot(HaveOccurred())
 
-  g.Expect(d).To(Equal(indicator.Document{
-    APIVersion: "v0",
-    Product:    "well-performing-component",
-    Version:    "0.0.1",
-    Metadata:   map[string]string{"deployment": "well-performing-deployment"},
-    Indicators: []indicator.Indicator{
-      {
-        Name:   "test_performance_indicator",
-        PromQL: "prom",
-        Thresholds: []indicator.Threshold{
-          {
-            Level:    "warning",
-            Operator: indicator.GreaterThanOrEqualTo,
-            Value:    50,
-          },
-        },
-        Documentation: map[string]string{
-          "title":                "Test Performance Indicator",
-          "description":          "This is a valid markdown description.",
-          "recommended_response": "Panic!",
-          "threshold_note":       "Threshold Note Text",
-        },
-      },
-    },
-    Documentation: indicator.Documentation{
-      Title:       "Monitoring Test Product",
-      Description: "Test description",
-      Sections: []indicator.Section{{
-        Title:       "Test Section",
-        Description: "This section includes indicators and metrics",
-        Indicators: []indicator.Indicator{{
-          Name:   "test_performance_indicator",
-          PromQL: "prom",
-          Thresholds: []indicator.Threshold{
-            {
-              Level:    "warning",
-              Operator: indicator.GreaterThanOrEqualTo,
-              Value:    50,
-            },
-          },
-          Documentation: map[string]string{
-            "title":                "Test Performance Indicator",
-            "description":          "This is a valid markdown description.",
-            "recommended_response": "Panic!",
-            "threshold_note":       "Threshold Note Text",
-          },
-        }},
-      }},
-    },
-  }))
+	g.Expect(d).To(Equal(indicator.Document{
+		APIVersion: "v0",
+		Product:    "well-performing-component",
+		Version:    "0.0.1",
+		Metadata:   map[string]string{"deployment": "well-performing-deployment"},
+		Indicators: []indicator.Indicator{
+			{
+				Name:   "test_performance_indicator",
+				PromQL: "prom",
+				Thresholds: []indicator.Threshold{
+					{
+						Level:    "warning",
+						Operator: indicator.GreaterThanOrEqualTo,
+						Value:    50,
+					},
+				},
+				Documentation: map[string]string{
+					"title":                "Test Performance Indicator",
+					"description":          "This is a valid markdown description.",
+					"recommended_response": "Panic!",
+					"threshold_note":       "Threshold Note Text",
+				},
+			},
+		},
+		Documentation: indicator.Documentation{
+			Title:       "Monitoring Test Product",
+			Description: "Test description",
+			Sections: []indicator.Section{{
+				Title:       "Test Section",
+				Description: "This section includes indicators and metrics",
+				Indicators: []indicator.Indicator{{
+					Name:   "test_performance_indicator",
+					PromQL: "prom",
+					Thresholds: []indicator.Threshold{
+						{
+							Level:    "warning",
+							Operator: indicator.GreaterThanOrEqualTo,
+							Value:    50,
+						},
+					},
+					Documentation: map[string]string{
+						"title":                "Test Performance Indicator",
+						"description":          "This is a valid markdown description.",
+						"recommended_response": "Panic!",
+						"threshold_note":       "Threshold Note Text",
+					},
+				}},
+			}},
+		},
+	}))
 }
 
 func TestReturnsAnEmptyListWhenNoIndicatorsArePassed(t *testing.T) {
-  g := NewGomegaWithT(t)
+	g := NewGomegaWithT(t)
 
-  d, err := indicator.ReadIndicatorDocument([]byte(`---
+	d, err := indicator.ReadIndicatorDocument([]byte(`---
 indicators: []`))
-  g.Expect(err).ToNot(HaveOccurred())
+	g.Expect(err).ToNot(HaveOccurred())
 
-  g.Expect(d.Indicators).To(HaveLen(0))
+	g.Expect(d.Indicators).To(HaveLen(0))
 }
 
 func TestReturnsAConvertedIndicator(t *testing.T) {
-  g := NewGomegaWithT(t)
+	g := NewGomegaWithT(t)
 
-  d, err := indicator.ReadIndicatorDocument([]byte(`---
+	d, err := indicator.ReadIndicatorDocument([]byte(`---
 indicators:
 - name: test-kpi
   promql: prom
+  slo: 0.99
   thresholds:
   - lt: 0
     level: warning
@@ -122,57 +123,58 @@ indicators:
   - gt: 1.222225
     level: warning`))
 
-  g.Expect(err).ToNot(HaveOccurred())
+	g.Expect(err).ToNot(HaveOccurred())
 
-  g.Expect(d.Indicators).To(Equal([]indicator.Indicator{{
-    Name:   "test-kpi",
-    PromQL: "prom",
-    Thresholds: []indicator.Threshold{
-      {
-        Level:    "warning",
-        Operator: indicator.LessThan,
-        Value:    0,
-      },
-      {
-        Level:    "warning",
-        Operator: indicator.LessThanOrEqualTo,
-        Value:    1.2,
-      },
-      {
-        Level:    "warning",
-        Operator: indicator.EqualTo,
-        Value:    0.2,
-      },
-      {
-        Level:    "warning",
-        Operator: indicator.NotEqualTo,
-        Value:    123,
-      },
-      {
-        Level:    "warning",
-        Operator: indicator.GreaterThanOrEqualTo,
-        Value:    642,
-      },
-      {
-        Level:    "warning",
-        Operator: indicator.GreaterThan,
-        Value:    1.222225,
-      },
-    },
-  }}))
+	g.Expect(d.Indicators).To(Equal([]indicator.Indicator{{
+		Name:   "test-kpi",
+		PromQL: "prom",
+		SLO:    0.99,
+		Thresholds: []indicator.Threshold{
+			{
+				Level:    "warning",
+				Operator: indicator.LessThan,
+				Value:    0,
+			},
+			{
+				Level:    "warning",
+				Operator: indicator.LessThanOrEqualTo,
+				Value:    1.2,
+			},
+			{
+				Level:    "warning",
+				Operator: indicator.EqualTo,
+				Value:    0.2,
+			},
+			{
+				Level:    "warning",
+				Operator: indicator.NotEqualTo,
+				Value:    123,
+			},
+			{
+				Level:    "warning",
+				Operator: indicator.GreaterThanOrEqualTo,
+				Value:    642,
+			},
+			{
+				Level:    "warning",
+				Operator: indicator.GreaterThan,
+				Value:    1.222225,
+			},
+		},
+	}}))
 }
 
 func TestReturnsAnErrorIfTheYAMLIsUnparsable(t *testing.T) {
-  g := NewGomegaWithT(t)
+	g := NewGomegaWithT(t)
 
-  _, err := indicator.ReadIndicatorDocument([]byte(`--`))
-  g.Expect(err).To(HaveOccurred())
+	_, err := indicator.ReadIndicatorDocument([]byte(`--`))
+	g.Expect(err).To(HaveOccurred())
 }
 
 func TestReturnsAnErrorIfAThresholdHasNoValue(t *testing.T) {
-  g := NewGomegaWithT(t)
+	g := NewGomegaWithT(t)
 
-  _, err := indicator.ReadIndicatorDocument([]byte(`---
+	_, err := indicator.ReadIndicatorDocument([]byte(`---
 indicators:
 - name: test-kpi
   description: desc
@@ -180,13 +182,13 @@ indicators:
   thresholds:
   - level: warning
   `))
-  g.Expect(err).To(HaveOccurred())
+	g.Expect(err).To(HaveOccurred())
 }
 
 func TestReturnsAnErrorIfAThresholdHasABadFloatValue(t *testing.T) {
-  g := NewGomegaWithT(t)
+	g := NewGomegaWithT(t)
 
-  _, err := indicator.ReadIndicatorDocument([]byte(`---
+	_, err := indicator.ReadIndicatorDocument([]byte(`---
 indicators:
 - name: test-kpi
   description: desc
@@ -195,14 +197,14 @@ indicators:
   - gte: abs
     level: warning
   `))
-  g.Expect(err).To(HaveOccurred())
+	g.Expect(err).To(HaveOccurred())
 }
 
 func TestReturnsErrors(t *testing.T) {
-  t.Run("if section references non-existent indicator", func(t *testing.T) {
-    g := NewGomegaWithT(t)
+	t.Run("if section references non-existent indicator", func(t *testing.T) {
+		g := NewGomegaWithT(t)
 
-    _, err := indicator.ReadIndicatorDocument([]byte(`---
+		_, err := indicator.ReadIndicatorDocument([]byte(`---
 apiVersion: v0
 product: my-product
 version: 1
@@ -216,6 +218,6 @@ documentation:
     indicators:
     - not_found
   `))
-    g.Expect(err).To(MatchError(ContainSubstring("documentation.sections[0].indicators[0] references non-existent indicator")))
-  })
+		g.Expect(err).To(MatchError(ContainSubstring("documentation.sections[0].indicators[0] references non-existent indicator")))
+	})
 }
