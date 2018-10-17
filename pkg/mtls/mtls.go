fb71c924d249f8a57ecf1d591c217e86c7137cc1
David Timm <dtimm@pivotal.io>
2018-10-17 10:27:07 -0600

    add ciphers and TLS version to client

--- pkg/mtls/mtls.go
+++ pkg/mtls/mtls.go
@@ -61,8 +61,21 @@ func NewClient(clientCert, clientKey, rootCACert string) (*http.Client, error) {
 
 	// Setup HTTPS client
 	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert},
-		RootCAs:      caCertPool,
+		Certificates:             []tls.Certificate{cert},
+		RootCAs:                  caCertPool,
+		MinVersion:               tls.VersionTLS12,
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		},
 	}
 	tlsConfig.BuildNameToCertificate()
 	client := &http.Client{
