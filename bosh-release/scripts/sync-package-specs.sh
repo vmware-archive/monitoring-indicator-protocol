6c70643fc7f24e1f47656699e6f15f82d5e07b08
Joseph Rodriguez <jrodriguez@pivotal.io>
2019-01-09 08:45:50 -0700

    merges bosh release config

---
+++ bosh-release/scripts/sync-package-specs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+go get github.com/vito/gosub
+
+if [ ! $(which gosub) ]; then
+    echo "Gosub required to update dependencies in bosh/*/spec files."
+    echo 'Please install with `go get github.com/vito/gosub`'
+    exit 1
+fi
+
+function sync_package() {
+  bosh_pkg=${1}
+
+  shift
+
+  (
+    set -e
+
+    cd packages/${bosh_pkg}
+
+    {
+      cat spec | grep -v '# gosub'
+      gosub list "$@" | \
+        sed -e 's|\(.*\)|- \1/*.go # gosub|g'
+    } > spec.new
+
+    mv spec.new spec
+
+  )
+}
+
+sync_package indicator-registry -app code.cloudfoundry.org/indicators/cmd/registry &
+sync_package indicator-registration-agent -app code.cloudfoundry.org/indicators/cmd/registry_agent &
+
+wait
