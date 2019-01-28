1d4061b11472b861577fab3b26b1b0b530ad5cad
Joseph Rodriguez <jrodriguez@pivotal.io>
2019-01-28 16:58:58 -0700

    move to pivotal org
    Signed-off-by: David Timm <dtimm@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -ef
 
-if [[ $# -ne 1 ]];
-then
-    echo "Incorrect usage: $0 pipeline-name"
-    exit 1
-fi
-
 set -eu
 
 lpass ls > /dev/null # check that we're logged in
 
-echo setting pipeline for "$1"
-fly -t superpipe set-pipeline -p "$1" \
-    -c "pipelines/$1.yml" \
+fly -t superpipe set-pipeline -p indicator-protocol \
+    -c "pipelines/indicator_protocol.yml" \
     --load-vars-from <(lpass show --notes "Shared-Event Producer (Pivotal Only)/concourse.yml")
