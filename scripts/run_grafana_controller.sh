2fcfdcded05e21a5fb716261bb85bf88e420286c
Joseph Rodriguez <jrodriguez@pivotal.io>
2019-02-21 11:50:29 -0700

    remove unused variables in controller scripts

--- scripts/run_grafana_controller.sh
+++ scripts/run_grafana_controller.sh
@@ -3,9 +3,7 @@ set -efu
 
 export CERTS=./test_fixtures
 export REGISTRY_PORT=8091
-export REGISTRY_HOST="https://localhost:${REGISTRY_PORT}"
-export DASHBOARD_PORT=8092
-export INDICATOR_DOCUMENTS='./example_indicators.yml'
+export REGISTRY_HOST="https://localhost:8091"
 
 echo "Running grafana agent"
 go run cmd/grafana_dashboard_controller/main.go \
@@ -14,6 +12,5 @@ go run cmd/grafana_dashboard_controller/main.go \
   -tls-pem-path ${CERTS}/client.pem \
   -tls-key-path ${CERTS}/client.key \
   -tls-root-ca-pem ${CERTS}/root.pem \
-  -tls-server-cn localhost \ &
+  -tls-server-cn localhost 
 
-wait
--- scripts/run_prometheus_controller.sh
+++ scripts/run_prometheus_controller.sh
@@ -3,9 +3,7 @@ set -efu
 
 export CERTS=./test_fixtures
 export REGISTRY_PORT=8091
-export REGISTRY_HOST="https://localhost:${REGISTRY_PORT}"
-export DASHBOARD_PORT=8092
-export INDICATOR_DOCUMENTS='./example_indicators.yml'
+export REGISTRY_HOST="https://localhost:8091"
 
 echo "Running prometheus agent"
 go run cmd/prometheus_alert_controller/main.go \
@@ -15,6 +13,5 @@ go run cmd/prometheus_alert_controller/main.go \
   -tls-key-path ${CERTS}/client.key \
   -tls-root-ca-pem ${CERTS}/root.pem \
   -tls-server-cn localhost \
-  -prometheus http://localhost:9090 \ &
+  -prometheus http://localhost:9090
 
-wait
