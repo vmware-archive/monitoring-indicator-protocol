305c9e197cd5097949ebc010baa1e159cd3d97d2
Mark Douglass <mdouglass@pivotal.io>
2019-04-29 14:32:24 -0600

    Use credhub for pipeline secrets to keep the pipes secure
    Signed-off-by: Vera Reynolds <vreynolds@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -6,5 +6,4 @@ set -eu
 lpass ls > /dev/null # check that we're logged in
 
 fly -t indipro set-pipeline -p indicator-protocol \
-    -c "pipelines/indicator_protocol.yml" \
-    --load-vars-from <(lpass show --notes "Shared-Event Producer (Pivotal Only)/concourse.yml")
+    -c "pipelines/indicator_protocol.yml"
