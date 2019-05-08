8d1d25c733b799217e435a71a73d850ed426b255
Vera Reynolds <vreynolds@pivotal.io>
2019-05-08 11:07:39 -0600

    added create-dev-release job
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-set -ef
 
-set -eu
-
-lpass ls > /dev/null # check that we're logged in
+set -efu
 
 fly -t indipro set-pipeline -p indicator-protocol \
     -c "pipelines/indicator_protocol.yml"
