dae7bafccfa8b11ef1b7702268666d5d992be17e
Vera Reynolds <vreynolds@pivotal.io>
2019-07-05 10:37:14 -0600

    Added html as a valid format option for cli

--- cmd/format/main.go
+++ cmd/format/main.go
@@ -17,7 +17,7 @@ import (
 
 func main() {
 	l := log.New(os.Stderr, "", 0)
-	outputFormat := flag.String("format", "bookbinder", "output format [bookbinder,prometheus-alerts,grafana]")
+	outputFormat := flag.String("format", "bookbinder", "output format [htlm,bookbinder,prometheus-alerts,grafana]")
 	metadata := flag.String("metadata", "", "metadata to override (e.g. --metadata deployment=my-test-deployment,source_id=metric-forwarder)")
 	indicatorsFilePath := flag.String("indicators", "", "indicators YAML file path")
 
